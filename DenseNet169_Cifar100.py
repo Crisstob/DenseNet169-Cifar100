7cb163b64d532c8cfb7e9377529d45e0ef24bec0
Cristóbal Fernández Véliz <49541906+Crisstob@users.noreply.github.com>
2023-06-12 16:54:19 -0400

    Update

--- DenseNet169_Cifar100.py
+++ DenseNet169_Cifar100.py
@@ -25,7 +25,7 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 from sklearn.model_selection import KFold, StratifiedKFold
-from matplotlib import pyplot as plt
+
 #import pandas as pd
 import numpy as np
 
@@ -41,7 +41,7 @@ img_width = 32
 batch_size = 128
 epochs = 150
 
-lr = 0.005
+lr = 0.001
 
 no_classes = 100
 
@@ -52,7 +52,7 @@ no_classes = 100
 # In[3]:
 
 
-new_adam = Adam(learning_rate=2e-5)
+new_adam = Adam(learning_rate=lr)
 new_sgd = SGD(learning_rate=lr, momentum=0.9)
 old_sgd = tf.keras.optimizers.SGD(learning_rate=lr, momentum = 0.9)
 
@@ -63,8 +63,8 @@ old_sgd = tf.keras.optimizers.SGD(learning_rate=lr, momentum = 0.9)
 # In[4]:
 
 
-def make_model_densenet169(weights=None):
-    model_d = DenseNet169(weights=weights, include_top=False, input_shape=(32, 32, 3))
+def make_model_densenet169():
+    model_d = DenseNet169(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
 
     x = model_d.output
 
@@ -81,142 +81,50 @@ def make_model_densenet169(weights=None):
     model = Model(inputs = model_d.input, outputs=preds)
     
     # Compilar el modelo
-    model.compile(optimizer=old_sgd, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=new_adam, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     
     return model
 
 
 # #Carga de CIFAR100 y Stratified 10 Fold 
 
-# In[6]:
+# In[5]:
 
 
 # import cifar 100 data
 # The data, split between train and test sets:
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
 
-#print('x_train shape:', x_train.shape)
-#print('y_train shape:', y_train.shape)
-#print('x_test shape:', x_test.shape)
-#print('y_test shape:', y_test.shape)
+
 # Parse numbers as floats
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+
+#y_train = tf.keras.utils.to_categorical(y_train, num_classes=100)
+#y_test = tf.keras.utils.to_categorical(y_test, num_classes=100)
 
 # Normalize data
 x_train = x_train / 255
 x_test = x_test / 255
 
 
-# In[11]:
+# In[6]:
 
 
-#Merge inputs and targets
-
-#inputs = np.concatenate((x_train,x_test), axis=0)
-#targets = np.concatenate((y_train, y_test), axis = 0)
-
-
-# In[7]:
-
-
-#Define the K-fold cross validator
-kfold = StratifiedKFold(n_splits = 10, shuffle = True, random_state = 123)
+reduce_lr = ReduceLROnPlateau(monitor='accuracy', factor=0.1, patience=5, verbose=1, min_lr=0.0001)
+# Entrenar el modelo
+model = make_model_densenet169()
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), callbacks=[reduce_lr])
 
 
 # #Resultados
 
-# In[8]:
+# In[7]:
 
 
 resultados = {'accuracy':[], 'val_accuracy':[], 'loss':[], 'val_loss':[]}
-
-
-# #Stratified 10 Fold Cross Validation
-
-# In[15]:
-
-
-fold_no = 1
-for train,test in kfold.split(x_train,y_train):
-    print(x_train[train].shape)
-    print(y_train[train].shape)
-    print(x_train[test].shape)
-    print(y_train[test].shape)
-    fold_no+=1
-
-
-# In[17]:
-
-
-fold_no = 1
-for train,test in kfold.split(x_train,y_train):
-  #Configurar conjunto de datos para rendimiento
-  #AUTOTUNE = tf.data.AUTOTUNE
-  #train = train.cache().prefetch(buffer_size = AUTOTUNE)
-  #test = test.cache().prefetch(buffer_size = AUTOTUNE)
-
-  #Crear modelo
-  model = make_model_densenet169()
-  model._name = "Model_"+str(fold_no)
-  # Generate a print
-  print('------------------------------------------------------------------------')
-  print(f'Training for fold {fold_no} ...')
-
-  #ReduceLROnPlateau
-  anne = ReduceLROnPlateau(monitor='accuracy', factor=0.1, patience=5, verbose=1, min_lr=1e-3)
-
-  # Save best model Checkpoint
-  best_model_dir = "chekpoint_densenet_169/model_cifar_"+str(fold_no)+".h5"
-  print("DIR Save Model:", best_model_dir)
-  checkpoint = ModelCheckpoint(best_model_dir, verbose=1, save_best_only=True)
-
-  #Entrenar el modelo
-  history = model.fit(x_train[train], y_train[train], validation_data= (x_train[test], y_train[test]),epochs=epochs, batch_size=batch_size, callbacks=[anne])
-
-  # Guardar el modelo / (pesos)
-  model.save(best_model_dir)
-
-# Guardar resultados
-  resultados['accuracy'].append(history.history['accuracy'])
-  resultados['val_accuracy'].append(history.history['val_accuracy'])
-  resultados['loss'].append(history.history['loss'])
-  resultados['val_loss'].append(history.history['val_loss'])
-
-  model = None
-
-
-  fold_no = fold_no + 1
-
-
-# #Visualizar Datos
-
-# In[18]:
-
-
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.savefig('training_graphs.png', bbox_inches='tight')
-plt.show()
+best_model_dir = "chekpoint_densenet_169/model_cifar_100.h5"
+model.save(best_model_dir)
 
 
 # In[ ]:
